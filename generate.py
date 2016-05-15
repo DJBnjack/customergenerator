bcb2ae2e0c0d8a34062de84667e3ce42c0f7076f
Johannes Bertens <johannes.bertens@live.nl>
2016-05-15 13:09:56 +0200

    Lets generate 100000 customers

--- generate.py
+++ generate.py
@@ -1,6 +1,8 @@
 import csv
 import random
 import json
+import sys
+
 
 with open('data/last-names.csv', 'rb') as f:
     last_names = list(csv.reader(f))
@@ -11,11 +13,11 @@ emailProviders = ['gmail.com', 'hotmail.com', 'live.com', 'live.nl', 'tele2.nl',
 cities = ['Rotterdam', 'Utrecht', 'Delft', 'Amsterdam', 'Breda', 'Groningen', 'Schiedam', 'Rijswijk']
 
 customers = []
-for x in range(0, 10):
+for x in range(0, 100000):
     customer = {}
     customer['initial'] = random.choice(capitalStrings)
     customer['gender'] = random.choice(genderStrings)
-    customer['lastname'] = last_names[random.randint(0,len(last_names))][0]
+    customer['lastname'] = last_names[random.randint(0,len(last_names)-1)][0]
     customer['postcode'] = str(random.randint(0,9999)) + random.choice(capitalStrings) + random.choice(capitalStrings)
     customer['street'] = "RandomStreet"
     customer['city'] = random.choice(cities)
@@ -24,6 +26,14 @@ for x in range(0, 10):
     customer['mobile'] = "06-" + str(random.randint(11111111,99999999))
     customer['email'] = customer['initial'].lower() + "." + customer['lastname'].lower() + "@" + random.choice(emailProviders)
     customers.append(customer)
+    
+    if x % 10000 == 0:
+        sys.stdout.write('.')
+
+print "Done generating!"
+print "Saving file..."
 
 with open('c:/temp/out.txt', 'w') as outfile:
     json.dump(customers, outfile, sort_keys = True, indent = 4)
+    
+print "Done!"
